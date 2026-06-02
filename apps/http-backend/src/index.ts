9836e0618baf1db858fed6196c98114ecb3cf8c6
rahul bauri <kumarrahulbauri2000@gmail.com>
2026-06-02 15:58:39 +0530

    added common folder for zod schemas

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -3,16 +3,35 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 import { JWT_SECRET } from '@repo/backend-common/config';
+import {
+  CreateUserSchema,
+  SigninUserSchema,
+  CreateRoomSchema,
+} from '@repo/common/types';
 import jwt from 'jsonwebtoken';
 import { middleware } from './middleware';
 
 const app = express();
 
 app.post('/signup', (req, res) => {
+  const data = CreateUserSchema.safeParse(req.body);
+  if (!data.success) {
+    return res.json({
+      message: 'Incorrect inputs',
+    });
+  }
+
   res.json({ userId: 123 });
 });
 
 app.post('/signin', (req, res) => {
+  const data = SigninUserSchema.safeParse(req.body);
+  if (!data.success) {
+    return res.json({
+      message: 'Incorrect inputs',
+    });
+  }
+
   const userId = '123';
   const token = jwt.sign({ userId }, JWT_SECRET as string);
 
@@ -20,6 +39,13 @@ app.post('/signin', (req, res) => {
 });
 
 app.post('/room', middleware, (req, res) => {
+  const data = CreateRoomSchema.safeParse(req.body);
+  if (!data.success) {
+    return res.json({
+      message: 'Incorrect inputs',
+    });
+  }
+
   res.json({
     roomId: 123,
   });
